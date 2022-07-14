a6a7b02741b95b2c98c73475b1cb3c4edd40bcb6
Daksh Lohar <dakshlohar4@gmail.com>
2022-07-14 12:58:19 +0530

    :construction: Add Cors Type and remove Jsonify

--- expressAPP/src/app.ts
+++ expressAPP/src/app.ts
@@ -1,25 +1,25 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import * as cors from 'cors'
-import { addNumRouter } from './controllers/addNum.controller';
+import * as cors from "cors";
+import { addNumRouter } from "./controllers/addNum.controller";
 
 // Our Express APP config
 const app = express();
 app.set("port", process.env.PORT || 3000);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 let corsOptions = {
-   origin : ['*'], // * for all origin
-}
-  
-app.use(cors(corsOptions))
+  origin: ["*"], // * for all origin
+};
+
+app.use(cors());
 
 // Register API Endpoints of controllers here
 app.get("/", (req, res) => {
   res.send("Hi");
 });
-app.use('/', addNumRouter)
+app.use("/", addNumRouter);
 
 // export our app
 export default app;
--- expressAPP/src/controllers/addNum.controller.ts
+++ expressAPP/src/controllers/addNum.controller.ts
@@ -6,9 +6,8 @@ export const addNumRouter = express.Router();
 
 // Make Endpoints Here
 addNumRouter.post("/add/", (req: Request, res: Response) => {
-	res.header('Access-Control-Allow-Origin', req.headers.origin);
-	const num1: string = req.body.num1;
-	const num2: string = req.body.num2;
-	const result = num1 + num2;
-	res.json(JSON.stringify({ result: 3 }));
+  const num1: string = req.body.num1;
+  const num2: string = req.body.num2;
+  const result = num1 + num2;
+  res.json({ result: 3 });
 });
